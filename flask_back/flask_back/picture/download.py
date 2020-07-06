b7a444c3d9216bd4978cb8cb7c6aaa5a2ea86bb5
white <2916144319@qq.com>
2020-07-06 07:32:31 +0000

    更改图片上传与图片生成路径不兼容的问题

--- flask_back/flask_back/picture/download.py
+++ flask_back/flask_back/picture/download.py
@@ -8,9 +8,8 @@ from flask_back.constant import PicturePath
 
 bp = Blueprint('picture_download', __name__, url_prefix='/picture')
 
-
 @bp.route('/download/<string:pic_name>', methods=['GET'])
-def download(pic_name):
+def download_one(pic_name):
     if request.method == 'GET':
         if os.path.exists(os.path.join(PicturePath, pic_name)):
             return send_from_directory(os.path.join(os.getcwd(), PicturePath), pic_name, as_attachment=True)
@@ -20,12 +19,45 @@ def download(pic_name):
         return '<h1>Wrong Request Function</h1>'
 
 @bp.route('/show/<string:pic_name>', methods=['GET'])
-def show(pic_name):
+def show_one(pic_name):
     if request.method == 'GET':
         if pic_name is None:
             return '<h1>Wrong Request Function</h1><h2>You didn\'t Input The Picture Path'
             pass
         else:
+            if not os.path.exists(os.path.join(PicturePath, '%s' % pic_name)):
+                return '<h1>NOT FOUND</h1>'
+            image_data = open(os.path.join(PicturePath, '%s' % pic_name), 'rb').read()
+            response = make_response(image_data)
+            response.headers['Content-Type'] = 'image/png'
+            return response
+    else:
+        return '<h1>Wrong Request Function</h1>'
+        pass
+
+
+
+@bp.route('/download/<string:pic_name>/<string:pic>', methods=['GET'])
+def download(pic_name,pic):
+    if request.method == 'GET':
+        if pic is not None and not pic == '':
+                pic_name = os.path.join(pic_name, pic)
+        if os.path.exists(os.path.join(PicturePath, pic_name)):
+            return send_from_directory(os.path.join(os.getcwd(), PicturePath), pic_name, as_attachment=True)
+        else:
+            return '<h1>NOT FOUND</h1>'
+    else:
+        return '<h1>Wrong Request Function</h1>'
+
+@bp.route('/show/<string:pic_name>/<string:pic>', methods=['GET'])
+def show(pic_name,pic):
+    if request.method == 'GET':
+        if pic_name is None:
+            return '<h1>Wrong Request Function</h1><h2>You didn\'t Input The Picture Path'
+            pass
+        else:
+            if pic is not None and not pic == '':
+                pic_name = os.path.join(pic_name, pic)
             if not os.path.exists(os.path.join(PicturePath, '%s' % pic_name)):
                 return '<h1>NOT FOUND</h1>'
             image_data = open(os.path.join(PicturePath, '%s' % pic_name), 'rb').read()
